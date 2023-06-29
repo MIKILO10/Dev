a6b5a323f7a341ca32aa2ca190d4dc2482d665f3
Gonzalo Mamaní López <gonzalomamani.ucse10@gmail.com>
2023-06-28 22:16:15 -0300

    Fix: Estructura del proyecto

--- www/forms/contact.php
+++ www/forms/contact.php
@@ -3,65 +3,51 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../src/Exception.php';
-require '../src/PHPMailer.php';
-require '../src/SMTP.php';
+require './src/Exception.php';
+require './src/PHPMailer.php';
+require './src/SMTP.php';
+
+// Configuración de PHPMailer
 $mail = new PHPMailer();
-$mail->IsSMTP(); // habilita SMTP
-#$mail->SMTPDebug = 2; // debugging: 1 = errores y mensajes, 2 = sólo mensajes
-$mail->SMTPAuth = true; // auth habilitada
-$mail->SMTPSecure = 'tls'; // transferencia segura REQUERIDA para Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 587; // or 465
-$mail->IsHTML(true);
-$mail->Username = "spike.jujuy10@gmail.com";
-$mail->Password = "tmqqcaueemrpyqyj";
-$mail->SetFrom($_POST['email'],$_POST['name']);
+$mail->isSMTP(); // Habilita SMTP
+$mail->SMTPDebug = 1; // Establece el nivel de depuración a 0 para no mostrar mensajes
+$mail->SMTPAuth = true; // Habilita la autenticación SMTP
+$mail->SMTPSecure = 'tls'; // Establece el tipo de seguridad (tls o ssl)
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587; // Puerto SMTP (tls: 587, ssl: 465)
+$mail->isHTML(true);
+
+$mail->Username = 'spike.jujuy10@gmail.com';
+$mail->Password = 'tmqqcaueemrpyqyj';
+$mail->setFrom($_POST['email'], $_POST['name']);
 $mail->Subject = $_POST['subject'];
 $mail->Body = $_POST['message'];
-$mail->AddAddress("spike.jujuy10@gmail.com");
+$mail->addAddress('spike.jujuy10@gmail.com');
 
-if(!$mail->Send()) {
-   echo '<script type="text/javascript">'
-   . 'Swal.fire({'
-   . 'icon: \'error\','
-   . 'title: \'Email\','
-   . 'text: \'No Enviado!\'});'
-   . '</script>'
-   . '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>'
-   . '<script>'
-   . 'setTimeout( function() { window.location.href = "https://dev-jujuy.net.ar"; }, 2000 );'
-   . '</script>';
+// Intenta enviar el correo electrónico
+if (!$mail->send()) {
+    // Error al enviar el correo electrónico
+    echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20"></script>';
+    echo '<script type="text/javascript">
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "No se pudo enviar el correo electrónico."
+            }).then(function() {
+                window.location.href = "https://dev-jujuy.net.ar";
+            });
+        </script>';
 } else {
-   echo '<script type="text/javascript">'
-   . 'Swal.fire('
-   . '\'Enviado\','
-   . '\'Correctamente\','
-   . '\'Gracias\''
-   . ');'
-   . '</script>'
-   . '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>'
-   . '<script>'
-   . 'setTimeout( function() { window.location.href = "https://dev-jujuy.net.ar"; }, 2000 );'
-   . '</script>';
+    // Correo electrónico enviado correctamente
+    echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20"></script>';
+    echo '<script type="text/javascript">
+            Swal.fire({
+                icon: "success",
+                title: "Éxito",
+                text: "Correo electrónico enviado correctamente."
+            }).then(function() {
+               window.location.href = "https://dev-jujuy.net.ar";
+            });
+        </script>';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
